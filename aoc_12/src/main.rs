912c9a9968391a6d9e7c284ba695dc6a04c84185
Dik van Genuchten <dikvangenuchten@gmail.com>
2021-12-12 21:16:26 +0100

    Day 12 part 2

--- aoc_12/src/main.rs
+++ aoc_12/src/main.rs
@@ -26,40 +26,25 @@ end-SM
 SM-uq",
     );
 
-    let paths = find_paths(input);
+    let paths_1 = find_paths(&input, recursion_criteria_part_1);
+    let paths_2 = find_paths(&input, recursion_criteria_part_2);
 
-    println!("n_paths: {:?}", paths.len())
+    println!("n_paths part 1: {:?}", paths_1.len());
+    println!("n_paths part 2: {:?}", paths_2.len());
 }
 
-struct Path {
-    path: Vec<String>,
-    _double_allowed: bool,
-}
-
-impl Path {
-    fn double_allowed(self: &Self) -> bool {
-        return self._double_allowed;
-    }
-
-    fn push(self: &mut Self, cave: String) {
-        if is_lowercase(&cave) {
-            if self.path.contains(&cave) {
-                self._double_allowed = false;
-            }
-        }
-        self.path.push(cave)
-    }
-}
-
-fn find_paths(input: String) -> HashSet<Vec<String>> {
+fn find_paths(
+    input: &String,
+    criterion: fn(&String, &Vec<String>) -> bool,
+) -> HashSet<Vec<String>> {
     let connections = create_connections(input);
     println!("connections: {:?}", connections);
-    let paths = search_paths_recursive(vec![String::from("start")], &connections);
+    let paths = search_paths_recursive(vec![String::from("start")], &connections, criterion);
 
     return paths;
 }
 
-fn create_connections(input: String) -> HashMap<String, HashSet<String>> {
+fn create_connections(input: &String) -> HashMap<String, HashSet<String>> {
     let mut connections = HashMap::<String, HashSet<String>>::new();
     for line in input.lines() {
         let (cave_1, cave_2) = line.split_once("-").unwrap();
@@ -86,6 +71,7 @@ fn is_uppercase(text: &String) -> bool {
 fn search_paths_recursive(
     path: Vec<String>,
     connections: &HashMap<String, HashSet<String>>,
+    criterion: fn(&String, &Vec<String>) -> bool,
 ) -> HashSet<Vec<String>> {
     let options = connections.get(path.last().unwrap()).unwrap();
 
@@ -95,16 +81,34 @@ fn search_paths_recursive(
         if next_cave == "end" {
             current_path.push(next_cave.clone());
             paths.insert(current_path);
-        } else if is_uppercase(next_cave) {
+        } else if next_cave == "start" {
+            continue;
+        } else if criterion(next_cave, &current_path) {
             current_path.push(next_cave.clone());
-            paths.extend(search_paths_recursive(current_path, connections));
-        } else if !current_path.contains(next_cave) {
-            current_path.push(next_cave.clone());
-            paths.extend(search_paths_recursive(current_path, connections));
+            paths.extend(search_paths_recursive(current_path, connections, criterion));
         }
     }
 
     return paths;
+}
+
+fn recursion_criteria_part_1(next_cave: &String, current_path: &Vec<String>) -> bool {
+    return is_uppercase(next_cave) || !current_path.contains(next_cave);
+}
+
+fn recursion_criteria_part_2(next_cave: &String, current_path: &Vec<String>) -> bool {
+    return is_uppercase(next_cave)
+        || !current_path.contains(next_cave)
+        || (is_lowercase(next_cave)
+            && current_path
+                .iter()
+                .filter(|x| is_lowercase(x))
+                .fold(HashMap::<&String, u128>::new(), |mut m, x| {
+                    *m.entry(x).or_insert(0) += 1;
+                    m
+                })
+                .iter()
+                .all(|(_cave, count)| count < &2));
 }
 
 #[cfg(test)]
@@ -200,7 +204,7 @@ b-end
             ],
         ]);
 
-        let paths = find_paths(input);
+        let paths = find_paths(&input, recursion_criteria_part_1);
 
         assert_eq!(expected, paths)
     }
@@ -217,26 +221,53 @@ pj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW",
         226
     )]
     fn test_path_count(#[case] input: String, #[case] count: usize) {
-        assert_eq!(find_paths(input).len(), count)
+        assert_eq!(find_paths(&input, recursion_criteria_part_1).len(), count)
     }
 
     #[rstest]
+    #[case("start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end", 36)]
     #[case(
         "dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc",
-        36
+        103
     )]
-    #[case("start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end", 103)]
     #[case(
         "fs-end\nhe-DX\nfs-he\nstart-DX\npj-DX\nend-zg\nzg-sl\nzg-pj\npj-he\nRW-he\nfs-DX
 pj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW",
         3509
     )]
     fn test_path_count_v2(#[case] input: String, #[case] count: usize) {
-        assert_eq!(find_paths(input).len(), count)
+        assert_eq!(find_paths(&input, recursion_criteria_part_2).len(), count)
+    }
+
+    #[test]
+    fn test_actual() {
+        let input = String::from(
+            "lg-GW
+pt-start
+pt-uq
+nx-lg
+ve-GW
+start-nx
+GW-start
+GW-nx
+pt-SM
+sx-GW
+lg-end
+nx-SM
+lg-SM
+pt-nx
+end-ve
+ve-SM
+TG-uq
+end-SM
+SM-uq",
+        );
+        assert_eq!(3708, find_paths(&input, recursion_criteria_part_1).len());
+        assert_eq!(93858, find_paths(&input, recursion_criteria_part_2).len());
     }
 
     #[bench]
-    fn bench_day_10(b: &mut Bencher) {
+    fn bench_day_12_part_1(b: &mut Bencher) {
         let input = String::from(
             "start-A
 start-b
@@ -246,6 +277,20 @@ b-d
 A-end
 b-end",
         );
-        // b.iter(|| aoc_12(&input));
+        b.iter(|| find_paths(&input, recursion_criteria_part_1))
+    }
+
+    #[bench]
+    fn bench_day_12_part_2(b: &mut Bencher) {
+        let input = String::from(
+            "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end",
+        );
+        b.iter(|| find_paths(&input, recursion_criteria_part_2))
     }
 }
