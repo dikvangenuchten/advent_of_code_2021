753865967a4162218420c7272bdf6f9958cc98dd
Dik van Genuchten <dikvangenuchten@gmail.com>
2021-12-14 20:26:59 +0100

    Add more benchmarks

--- aoc_14/benches/benchmark.rs
+++ aoc_14/benches/benchmark.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 extern crate aoc_14;
 use aoc_14::*;
@@ -9,16 +9,20 @@ fn speed_test_day_14(c: &mut Criterion) {
     c.bench_function("day_14_part_1", |b| {
         b.iter(|| aoc_14(black_box(&input_file), black_box(10)))
     });
-    c.bench_function("day_14_part_1_2", |b| {
-        b.iter(|| aoc_14(black_box(&input_file), black_box(20)))
-    });
-    c.bench_function("day_14_part_1_3", |b| {
-        b.iter(|| aoc_14(black_box(&input_file), black_box(30)))
-    });
     c.bench_function("day_14_part_2", |b| {
         b.iter(|| aoc_14(black_box(&input_file), black_box(40)))
     });
 }
 
-criterion_group!(benches, speed_test_day_14);
+fn speed_test_day_14_o_notation(c: &mut Criterion) {
+    let input_file = String::from("src/input");
+
+    for i in 1..=10 {
+        c.bench_with_input(BenchmarkId::new("day_14", i), &(10 * i), |b, i| {
+            b.iter(|| aoc_14(black_box(&input_file), black_box(*i)))
+        });
+    }
+}
+
+criterion_group!(benches, speed_test_day_14, speed_test_day_14_o_notation);
 criterion_main!(benches);
